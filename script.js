0a179c3a911adc8d74dd9c4a4076ded9f3d4bfb9
ZephyrPHS <129297391+ZephyrPHS@users.noreply.github.com>
2023-04-08 19:48:43 -0400

    Update script.js

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function() {
             // Replace the following lines with your actual login logic
             if (username === 'admin' && password === 'password123') {
                 alert('Login successful!');
-                window.location.href = url;
+                window.location.href = 'url';
             } else {
                 alert('Invalid username or password. Please try again.');
             }
