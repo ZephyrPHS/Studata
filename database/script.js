cac07a205c5d91a15ad92eace7007269cee3e533
ZephyrPHS <129297391+ZephyrPHS@users.noreply.github.com>
2023-05-24 08:39:31 -0400

    Update script.js

--- database/script.js
+++ database/script.js
@@ -1,9 +1,8 @@
-import { saveAs } from 'file-saver';
 // Sample student data
 let students = [
   { name: "John Doe", studentId: "123456" },
   { name: "Jane Smith", studentId: "789012" },
-]; 
+];
 // Function to render the student list
 function renderStudents() {
   const studentList = document.getElementById("student-list");
@@ -23,8 +22,8 @@ function renderStudents() {
     studentList.appendChild(row);
     id++;
   });		
-  //var blob = new Blob([studentText], { type: "text/plain;charset=utf-8" });
-  //saveAs(blob, "studentData.txt");
+  var blob = new Blob([students], { type: "text/plain;charset=utf-8" });
+  saveAs(blob, "studentData.txt");
 }
 function editStudent(editId) {
   const studentList = document.getElementById("student-list");
