9b1d5c0253dea7482a55150347b709321fea1666
ZephyrPHS <129297391+ZephyrPHS@users.noreply.github.com>
2023-05-24 08:43:04 -0400

    Update script.js

--- database/script.js
+++ database/script.js
@@ -122,18 +122,18 @@ function exportToCsv(filename, rows) {
     if (navigator.msSaveBlob) { // IE 10+
         navigator.msSaveBlob(blob, filename);
     } else {
-        var link = document.createElement("a");
-        if (link.download !== undefined) { // feature detection
-            // Browsers that support HTML5 download attribute
-            var url = URL.createObjectURL(blob);
-            link.setAttribute("href", url);
-            link.setAttribute("download", filename);
-            link.style.visibility = 'hidden';
-            document.body.appendChild(link);
-            link.click();
-            document.body.removeChild(link);
-        }
+var link = document.createElement("a");
+if (link.download !== undefined) { // feature detection
+    // Browsers that support HTML5 download attribute
+    var url = URL.createObjectURL(blob);
+    link.setAttribute("href", url);
+    link.setAttribute("download", filename);
+    link.style.visibility = 'hidden';
+    document.body.appendChild(link);
+    link.click();
+      document.body.removeChild(link);
     }
+  }
 }
 // Event listener for the form submission
 document.getElementById("export-student-data").addEventListener("submit", exportData);
