c4d8fbefaaa1c4e198a47035ac835884b4dc4891
ZephyrPHS <129297391+ZephyrPHS@users.noreply.github.com>
2023-05-25 09:35:42 -0400

    Update script.js

--- database/script.js
+++ database/script.js
@@ -3,7 +3,6 @@ let students = [
   { name: "John Doe", studentId: "123456" },
   { name: "Jane Smith", studentId: "789013" }
 ];
-renderStudents();
 let data = "Ricky Sun,144283/nSumeir Soni,999999";
 let array = data.split("\\n").map(function (line) {
     return line.split(",");
