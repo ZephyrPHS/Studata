0685b3bf92bdc79b048a7d926016083a05f64002
ZephyrPHS <129297391+ZephyrPHS@users.noreply.github.com>
2023-06-06 02:05:08 -0400

    Update script.js

--- database/script.js
+++ database/script.js
@@ -15,7 +15,7 @@ if (sessionStorage.getItem("token") === "adminpassword") {
   let students = [];
 
   // Check if data exists in firebase
-  database.ref('studentData').on('value', function(snapshot) {
+  database.ref('studentData').once('value', function(snapshot) {
     var firebaseData = snapshot.val();
     var csvFile = firebaseData.name;
     console.log(firebaseData);
