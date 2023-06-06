e9ca0f3d8b0f99ee3c9746475c52759503e4d050
ZephyrPHS <129297391+ZephyrPHS@users.noreply.github.com>
2023-06-06 10:37:22 -0400

    Update script.js

--- database/script.js
+++ database/script.js
@@ -14,7 +14,7 @@ if (sessionStorage.getItem("token") === "adminpassword") {
   // Session student data
   let students = [];
   var dataRef = database.ref('studentData');
-  dataRef.once('value', function(snapshot) {
+  dataRef.on('value', function(snapshot) {
     snapshot.forEach(function(childSnapshot) {
       var childData = childSnapshot.val();
 
