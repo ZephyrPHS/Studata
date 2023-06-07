b70c3053415bf7c8af1ef0d3a981d1ef447e8ebd
ZephyrPHS <129297391+ZephyrPHS@users.noreply.github.com>
2023-06-06 23:03:39 -0400

    Update script.js

--- signup/script.js
+++ signup/script.js
@@ -8,63 +8,71 @@ function isDuplicateUser(users, email, username) {
 
 document.addEventListener("DOMContentLoaded", function () {
   // Get the form element
-  const usernameInput = document.getElementById("username");
-  const emailInput = document.getElementById("email");
-  const passwordInput = document.getElementById("password");
+  const signUpForm = document.querySelector('.the-form');
 
-  // Perform validation
-  const username = usernameInput.value.trim();
-  const email = emailInput.value.trim();
-  const password = passwordInput.value.trim();
+  // Add event listener for form submission
+  signUpForm.addEventListener('submit', function (event) {
+    event.preventDefault(); // Prevent form submission
 
-  if (username === "" || email === "" || password === "") {
-    alert("Please enter username, email, and password.");
-    return;
-  }
+    // Get the input values
+    const usernameInput = document.getElementById("username");
+    const emailInput = document.getElementById("email");
+    const passwordInput = document.getElementById("password");
 
-  // Retrieve existing users from local storage
-  const existingUsersCSV = localStorage.getItem("users");
-  const existingUsers2D = Papa.parse(existingUsersCSV).data;
-  const existingUsers = [];
+    // Perform validation
+    const username = usernameInput.value.trim();
+    const email = emailInput.value.trim();
+    const password = passwordInput.value.trim();
 
-  // Convert existingUsers2D to an array of user objects
-  for (let i = 0; i < existingUsers2D.length; i++) {
-    const user = {
-      username: existingUsers2D[i][0],
-      email: existingUsers2D[i][1],
-      password: existingUsers2D[i][2],
+    if (username === "" || email === "" || password === "") {
+      alert("Please enter username, email, and password.");
+      return;
+    }
+
+    // Retrieve existing users from local storage
+    const existingUsersCSV = localStorage.getItem("users");
+    const existingUsers2D = Papa.parse(existingUsersCSV).data;
+    const existingUsers = [];
+
+    // Convert existingUsers2D to an array of user objects
+    for (let i = 0; i < existingUsers2D.length; i++) {
+      const user = {
+        username: existingUsers2D[i][0],
+        email: existingUsers2D[i][1],
+        password: existingUsers2D[i][2],
+      };
+      existingUsers.push(user);
+    }
+
+    // Check for duplicate email or username
+    if (isDuplicateUser(existingUsers, email, username)) {
+      alert("A user with the same email or username already exists.");
+      return;
+    }
+
+    // Create a new user object
+    const newUser = {
+      username: username,
+      email: email,
+      password: password,
     };
-    existingUsers.push(user);
-  }
 
-  // Check for duplicate email or username
-  if (isDuplicateUser(existingUsers, email, username)) {
-    alert("A user with the same email or username already exists.");
-    return;
-  }
+    // Add the new user to the existing users array
+    existingUsers.push(newUser);
 
-  // Create a new user object
-  const newUser = {
-    username: username,
-    email: email,
-    password: password,
-  };
+    // Convert the updated users array back to CSV format
+    const updatedUsersCSV = Papa.unparse(existingUsers);
 
-  // Add the new user to the existing users array
-  existingUsers.push(newUser);
+    // Update the users in local storage
+    localStorage.setItem("users", updatedUsersCSV);
 
-  // Convert the updated users array back to CSV format
-  const updatedUsersCSV = Papa.unparse(existingUsers);
+    // Clear the form inputs
+    usernameInput.value = "";
+    emailInput.value = "";
+    passwordInput.value = "";
 
-  // Update the users in local storage
-  localStorage.setItem("users", updatedUsersCSV);
-
-  // Clear the form inputs
-  usernameInput.value = "";
-  emailInput.value = "";
-  passwordInput.value = "";
-
-  alert(
-    "Account creation request has been sent to the RTSE and is waiting approval. You may close this page."
-  ); // send request to RTSE
+    alert(
+      "Account creation request has been sent to the RTSE and is waiting approval. You may close this page."
+    ); // send request to RTSE
+  });
 });
